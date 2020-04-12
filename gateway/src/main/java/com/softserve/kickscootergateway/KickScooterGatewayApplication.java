31919742a9b9644bbf7095f15caecf8f38bbc87a
root <root@mail.svagworks.me>
2020-04-12 12:33:45 +0000

    tes

---
+++ gateway/src/main/java/com/softserve/kickscootergateway/KickScooterGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscootergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickScooterGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickScooterGatewayApplication.class, args);
+    }
+
+}
